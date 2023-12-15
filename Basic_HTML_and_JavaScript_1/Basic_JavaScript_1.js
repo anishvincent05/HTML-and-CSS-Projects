b4bde8ed82448e287a9031fd8bfc5eb2efe84cd7
anishvincent05 <anishvincent@outlook.com>
2023-12-14 20:56:20 -0700

    Basic html and javascript 1

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,6 @@
+alert("Hello,World!");
+function My_First_Function(){
+			var string = "I'm canadian";
+			var result = string.fontcolor("red");
+			document.getElementById("canada").innerHTML = result;
+			}
